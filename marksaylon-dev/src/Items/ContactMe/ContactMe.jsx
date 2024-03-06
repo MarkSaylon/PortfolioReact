06537e6f9a37e31b760791f0d65d0be45cd4c956
unknown <marksaylon@gmail.com>
2024-03-06 14:08:44 +0800

    fixed target blank on links

--- marksaylon-dev/src/Items/ContactMe/ContactMe.jsx
+++ marksaylon-dev/src/Items/ContactMe/ContactMe.jsx
@@ -3,9 +3,9 @@ import styles from './ContactMe.module.css'
 function ContactMe(){
     return(
         <div className={styles.container}>
-            <a href="https://www.linkedin.com/in/mark-vincent-andrei-saylon-782b61248/" className={styles.links}>Linked In</a>
-            <a href="https://github.com/MarkSaylon" className={styles.links}>GitHub</a>
-            <a  href={`mailto:marksaylon@gmail.com?subject=Inquiry from Your Website`} className={styles.links}>Email</a>
+            <a href="https://www.linkedin.com/in/mark-vincent-andrei-saylon-782b61248/" target='blank' className={styles.links}>Linked In</a>
+            <a href="https://github.com/MarkSaylon" target='blank' className={styles.links}>GitHub</a>
+            <a  href={`mailto:marksaylon@gmail.com?subject=Inquiry from Your Website`} target='blank' className={styles.links}>Email me: marksaylon@gmail.com</a>
         </div>
     )
 
