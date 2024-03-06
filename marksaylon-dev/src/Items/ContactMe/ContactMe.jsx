1cdbd090b63748ba1de1e0aa24a52ec1586cb09c
unknown <marksaylon@gmail.com>
2024-03-06 14:05:56 +0800

    added Email

--- marksaylon-dev/src/Items/ContactMe/ContactMe.jsx
+++ marksaylon-dev/src/Items/ContactMe/ContactMe.jsx
@@ -5,7 +5,7 @@ function ContactMe(){
         <div className={styles.container}>
             <a href="https://www.linkedin.com/in/mark-vincent-andrei-saylon-782b61248/" className={styles.links}>Linked In</a>
             <a href="https://github.com/MarkSaylon" className={styles.links}>GitHub</a>
-            <a href="marksaylon@gmail.com" className={styles.links}>Email</a>
+            <a  href={`mailto:marksaylon@gmail.com?subject=Inquiry from Your Website`} className={styles.links}>Email</a>
         </div>
     )
 
